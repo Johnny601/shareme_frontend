c332426346849fe0646df51d6f5784ad8a788361
Johnny <johnnyyu601@gmail.com>
2024-09-23 00:57:30 +0800

    first commit

---
+++ src/component/index.js
@@ -0,0 +1,19 @@
+import Login from "./Login";
+import Sidebar from "./Sidebar";
+import UserProfile from "./UserProfile";
+import Navbar from "./Navbar";
+import Feed from "./Feed";
+import PinDetail from "./PinDetail";
+import CreatePin from "./CreatePin";
+import Search from "./Search";
+
+export {
+  Login,
+  Sidebar,
+  UserProfile,
+  Navbar,
+  Feed,
+  PinDetail,
+  CreatePin,
+  Search,
+};
